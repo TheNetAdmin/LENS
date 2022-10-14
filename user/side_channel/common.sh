08dcb4670ec46d15ce268ffa03747964d9ff9ef2
Zixuan Wang <zxwang42@gmail.com>
2022-10-13 23:08:44 -0700

    Update code comments for host setup

--- user/side_channel/common.sh
+++ user/side_channel/common.sh
@@ -6,17 +6,6 @@ check_mtrr cacheable
 export host_name=$(hostname | tr -d "[:space:]")
 
 case "${host_name}" in
-	AEP2)
-		export dram_dev="${dram_dev:-/dev/pmem0}"
-		export pmem_dev="${pmem_dev:-/dev/pmem13}"
-	;;
-	sdp)
-		export dram_dev="${dram_dev:-/dev/pmem0}"
-		export pmem_dev="${pmem_dev:-/dev/pmem1}"
-		export pmdk_path="/root/PMDK-Libs"
-		export pmdk_libpmemobj_cpp_path="${pmdk_path}/libpmemobj-cpp"
-		export dax_dev="${dax_dev:-/dev/dax1.2}"
-	;;
 	nv-4)
 		export dram_dev="${dram_dev:-/dev/pmem0}"
 		export pmem_dev="${pmem_dev:-/dev/pmem1}"
@@ -25,11 +14,6 @@ case "${host_name}" in
 		dax_attacker=$(ndctl list | jq '.[] | select(.name=="dax-attacker") | .chardev' | tr -d '"')
 		[ -z "${dax_attacker}" ] && dax_attacker="dax_dev_not_found"
 		export dax_dev="${dax_dev:-/dev/${dax_attacker}}"
-	;;
-	lens)
-		# lens vm
-		export dram_dev="${dram_dev:-/dev/pmem0}"
-		export pmem_dev="${pmem_dev:-/dev/pmem1}"
 	;;
 	*)
 		echo "Unknown host machine: $(hostname)"
