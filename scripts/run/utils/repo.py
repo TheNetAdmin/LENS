cfa857aff069a72ecf175bba9953fbfa46ca5e6b
Zixuan Wang <zxwang42@gmail.com>
2023-09-23 21:58:29 -0700

    "Auto commit [2023-09-23-21-58-29]"

---
+++ scripts/run/utils/repo.py
@@ -0,0 +1,24 @@
+from loguru import logger
+
+from .utils import Cmd, run_cmd, curr_time
+
+
+def status(remote_host=None, remote_dir=None):
+    if remote_host:
+        with Cmd().ssh(remote_host).cd(remote_dir) as c:
+            c("git status")
+    else:
+        run_cmd("git status")
+
+
+def commit_and_push():
+    with Cmd("git") as c:
+        c("add *")
+        res = c("diff-index HEAD --", capture_output=True, text=True)
+        num_diff_files = len(res.stdout.split("\n"))
+
+        if num_diff_files > 0:
+            logger.info(f"{num_diff_files} files modified, needs commit and push")
+            commit_msg = f"Auto commit [{curr_time()}]"
+            c(["commit", "-am", f'"{commit_msg}"'])
+            c("push")
